85dc1a4263008c5a5377c778e30d21531df49115
priyankamasiwal26025-dot <priyankamasiwal26025@gmail.com>
2025-09-01 11:17:29 +0530

    Update train.py

--- model/train.py
+++ model/train.py
@@ -5,9 +5,10 @@ import joblib
 
 # Load the dataset
 df = pd.read_csv("data/WA_Fn-UseC_-Telco-Customer-Churn.csv")
+
 # Preprocess the dataset
-X = data.drop('Churn', axis=1)
-y = data['Churn']
+X = df.drop('Churn', axis=1)
+y = df['Churn']
 
 # Split the data into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
