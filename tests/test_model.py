11937e164aa53b4f536623bccc34d7650bd5a26c
priyankamasiwal26025-dot <priyankamasiwal26025@gmail.com>
2025-10-27 23:02:23 +0530

    Initial Commit 1.3

--- tests/test_model.py
+++ tests/test_model.py
@@ -2,12 +2,14 @@ import unittest
 import joblib
 from sklearn.ensemble import RandomForestClassifier
 
+
 class TestModelTraining(unittest.TestCase):
-def test_model_training(self):
-model = joblib.load('model/Churn_model.pkl')
-self.assertIsInstance(model, RandomForestClassifier)
-
-self.assertGreaterEqual(len(model.feature_importances_), 4)
-
+	def test_model_training(self):
+		model = joblib.load('model/Churn_model.pkl')
+		self.assertIsInstance(model, RandomForestClassifier)
+		self.assertGreaterEqual(len(model.feature_importances_), 4)
+      
 if __name__ == '__main__':
-unittest.main()
+	unittest.main()
+
+ 
